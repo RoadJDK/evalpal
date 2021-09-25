39644f283aa1c55d4023aedf434b0d61e559da11
RoadJDK <realroadjdk@gmail.com>
2021-09-25 18:34:33 +0200

    link login with ui

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const { menubar } = require('menubar')
-const { Notification } = require('electron')
+const { Notification, ipcMain } = require('electron')
+const path = require('path')
 
 const AmazonCognitoIdentity = require('amazon-cognito-identity-js')
 global.fetch = require('node-fetch')
@@ -11,20 +12,30 @@ const poolData = {
 
 const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
 
-const mb = menubar({preloadWindow: true, icon: 'icons/icon.png', tooltip: 'EVALPAL', browserWindow: { height: 800, width: 600 }});
+const mb = menubar({preloadWindow: true, icon: 'icons/icon.png', tooltip: 'EVALPAL', browserWindow: { height: 800, width: 600, webPreferences: { nodeIntegration: true, contextIsolation: false, enableRemoteModule: true, preload: path.join(__dirname, 'preload.js')}}})
+
+let accessToken
+let idToken
+let refreshToken
 
 mb.on('ready', () => {
+    mb.window.webContents.openDevTools()
     console.log('app is ready');
     APIListener('praise')
-    //RegisterUser()
-    //LogIn()
-    //showNotification()
   });
 
 mb.on('hide', () => {
   console.log('window closed')
   mb.window.loadFile('index.html')
 })
+
+ipcMain.on('signup-message', (event, firstname, name, email, password) => {
+  RegisterUser(firstname, name, email, password)
+  });
+
+ipcMain.on('login-message', (event, email, password) => {
+  LogIn(email, password)
+  });
 
   function showNotification (title, body) {
     var notification = new Notification({ title: title, body: body, icon: 'icons/icon.png' })
@@ -36,48 +47,44 @@ mb.on('hide', () => {
     notification.show()
   }
 
-  function RegisterUser() {
+  function RegisterUser(name, firstname, email, password) {
     var attributeList = [];
-    attributeList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name:"name",Value:"user"}))
-    attributeList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name:"family_name",Value:"male"}))
-    attributeList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name:"email",Value:"sample@email.com"}))
+    attributeList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name:"name",Value:firstname}))
+    attributeList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name:"family_name",Value:name}))
+    attributeList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name:"email",Value:email}))
 
-    userPool.signUp('sample@email.com', 'SamplePassword123', attributeList, null, function(err, result){
+    userPool.signUp(email, password, attributeList, null, function(err, result){
         if (err) {
             console.log(err);
             return;
         }
+        console.log('signed up!')
         cognitoUser = result.user;
-        console.log('user name is ' + cognitoUser.getUsername());
     });
   }
 
-  function LogIn() {
+  function LogIn(email, password) {
     var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails({
-      Username : 'sample@email.com',
-      Password : 'SamplePassword123',
+      Username : email,
+      Password : password,
     });
 
     var userData = {
-      Username : 'sample@email.com',
+      Username : email,
       Pool : userPool
     };
     var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
     cognitoUser.authenticateUser(authenticationDetails, {
       onSuccess: function (result) {
-          console.log('access token + ' + result.getAccessToken().getJwtToken());
-          console.log('id token + ' + result.getIdToken().getJwtToken());
-          console.log('refresh token + ' + result.getRefreshToken().getToken());
+        console.log('logged in!')
+          accessToken = result.getAccessToken().getJwtToken();
+          idToken = result.getIdToken().getJwtToken();
+          refreshToken = result.getRefreshToken().getToken();
       },
       onFailure: function(err) {
           console.log(err);
       },
-
     });
-  }
-
-  function Test() {
-    alert('Alert in Electron');
   }
 
   function APIListener(dto1) {
