9e40f8afbb7be89687b4912be5ec465f0af837c1
RoadJDK <realroadjdk@gmail.com>
2021-09-25 10:33:15 +0200

    add basic alert

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 const { menubar } = require('menubar');
+const { Notification } = require('electron')
 
-const mb = menubar({icon: 'icons/icon.png'});
+const NOTIFICATION_TITLE = 'You got a new message!🎉'
+const NOTIFICATION_BODY = 'Open the app to see it'
+
+const mb = menubar({icon: 'icons/icon.png',
+tooltip: 'EVALPAL'});
 
 mb.on('ready', () => {
     console.log('app is ready');
+    showNotification()
   });
+
+  function showNotification () {
+    var notification = new Notification({ title: NOTIFICATION_TITLE, body: NOTIFICATION_BODY, icon: 'icons/icon.png' })
+    notification.show()
+    notification.on('click', (event, arg)=>{
+      console.log('clicked')
+      mb.showWindow()
+    })
+  }
