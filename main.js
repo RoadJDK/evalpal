29dd18dfcf1691cbe8e021a81e8d0171ed1f5ae9
RoadJDK <realroadjdk@gmail.com>
2021-09-25 02:40:04 +0200

    add menubar and icons

--- main.js
+++ main.js
@@ -1,23 +1,7 @@
-const { app, BrowserWindow } = require('electron')
+const { menubar } = require('menubar');
 
-function createWindow () {
-    
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600
-  })
+const mb = menubar({icon: 'icons/icon.png'});
 
-  mainWindow.loadFile('index.html')
-}
-
-app.whenReady().then(() => {
-  createWindow()
-
-  app.on('activate', function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
-
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
+mb.on('ready', () => {
+    console.log('app is ready');
+  });
